6e4434b290c3ed94399080f3a7b1073df0056935
Denis <nosovdenis14@gmail.com>
2024-12-11 16:45:28 +0300

    Connecting signals in script "hud"

--- hud.gd
+++ hud.gd
@@ -1,11 +1,33 @@
 extends CanvasLayer
 
+# Уведомляет узел `Main` о нажатии кнопки
+signal start_game
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Временно отображает сообщение
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+# Вызывается когда игрок проигрывает
+func show_game_over():
+	show_message("Игра окончена")
+	# Подождите, пока MessageTimer не завершит обратный отсчет.
+	await $MessageTimer.timeout
+	
+	$Message.text = "Увернись от Крипов"
+	$Message.show()
+	# Заведите таймер на один заход и дождитесь его окончания.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_message_timer_timeout():
+	$Message.hide()
