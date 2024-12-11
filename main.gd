c57e5396f6f32b2f745732339c20a9c750c0101e
Denis <nosovdenis14@gmail.com>
2024-12-11 17:00:42 +0300

    Changed the script "main"

--- main.gd
+++ main.gd
@@ -9,12 +9,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Приготовтесь")
 
 func _on_mob_timer_timeout():
 	
@@ -44,7 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
